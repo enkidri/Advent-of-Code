c4a63351212181560d0fdbe97157c13199f884d0
enkidri <60242641+enkidri@users.noreply.github.com>
2022-01-27 13:08:31 +0100

    Update day7.cc

--- Advent-of-Code-2022/day7.cc
+++ Advent-of-Code-2022/day7.cc
@@ -8,6 +8,46 @@
 #include <algorithm>
 using namespace std;
 
+int main_for_part_2(int argv, char** argc)
+{
+    vector<int> pos;
+    ifstream f{argc[1]};
+    string line{};
+    while(getline(f,line))
+    {
+        stringstream ss(line);
+        for (int i; ss >> i;)
+        {
+            pos.push_back(i);
+            if (ss.peek() == ',')
+            {
+                ss.ignore();
+            }
+        }
+    }
+
+    int max = *max_element(pos.begin(), pos.end());
+
+    int cost{};
+    vector<int> costs;
+    int average = 0;
+    for (int i=0; i < max; ++i)         //Brute fucking force motherfucker
+    {
+        cost = 0;
+        for (auto const& p:pos)
+        {
+            int dist = abs(p-average);
+            cost += dist*(1 + dist) / 2;
+        }
+        costs.push_back(cost);
+        ++average;
+    }
+
+    cout << "The minimal cost is " << *min_element(costs.begin(), costs.end()) << endl;
+
+    return 0;
+}
+
 
 int main(int argv, char** argc)
 {
