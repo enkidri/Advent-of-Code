4498d380e255888a7a3087dc906a3388b10b3ce5
enkidri <60242641+enkidri@users.noreply.github.com>
2022-01-27 12:56:59 +0100

    Update day2_1.cc

--- Advent-of-Code-2022/day2_1.cc
+++ Advent-of-Code-2022/day2_1.cc
@@ -3,8 +3,41 @@
 #include <string>
 using namespace std;
 
+void partTwo()
+{
+    ifstream f{"day2_input.txt"};
+    int forward{};
+    int depth{};
+    int aim{};
+    string command{};
+    int dist{};
+    if (f.is_open())
+    {
+        while(f >> command >> dist)
+        {
+            if (command == "forward")
+            {
+                forward += dist;
+                depth += dist*aim;
+            }
+            else if(command == "down")
+            {
+                aim += dist;
+            }
+            else if(command == "up")
+            {
+                aim -= dist;
+            }
+        }
+    }
 
-int main()
+    cout << "The position of the submarine is\nHorizontal: " << forward << "\n" <<
+              "Depth: " << depth << endl;
+
+    cout << "The product is: " << depth*forward << endl;
+}
+
+int partOne()
 {
     ifstream f{"day2_input.txt"};
     int forward{};
@@ -34,6 +67,11 @@ int main()
               "Depth: " << depth << endl;
 
     cout << "The product is: " << depth*forward << endl;
+}
 
+int main()
+{
+    partOne();
+    partTwo();
     return 0;
 }
